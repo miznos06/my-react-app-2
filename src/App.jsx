bfd85797e80b84b72d0560e3caeb4a01cdcb09f0
miznos <miznos06@gmail.com>
2026-03-10 22:10:58 +0900

    update before monorepo restructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from "react"
 import './App.css'
 
 function App() {
+  const [message, setMessage] = useState("")
   const [count, setCount] = useState(0)
+  const [input, setInput] = useState("")
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  // DynamoDBデータ取得
+  useEffect(() => {
+    fetch(`${API_URL}/hello`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        if (data.length > 0) {
+          setMessage(data[data.length - 1].message)
+        }
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  // DynamoDBへ保存
+  const sendMessage = async () => {
+
+    await fetch(`${API_URL}/hello`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: input
+      })
+    })
+
+    setInput("")
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React 6</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>API Test</h1>
+
+      <p>Latest message: {message}</p>
+
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="enter message"
+      />
+
+      <button onClick={sendMessage}>
+        Send message
+      </button>
+
+      <br /><br />
+
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
     </>
   )
 }
